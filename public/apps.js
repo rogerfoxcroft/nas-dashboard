480d76e73604e9cc8d985b6822b145ca3ab165f7
Roger Foxcroft <roger@softfox.com>
2026-01-16 14:02:49 +0000

    Centralize app config for iframe shell

---
+++ public/apps.js
@@ -0,0 +1,8 @@
+window.NAS_APPS = [
+  { name: 'Synology DSM', port: '5000', openInNewTab: true },
+  { name: 'Plex', port: '32400', path: '/web' },
+  { name: 'Transmission', port: '9091' },
+  { name: 'Radarr', port: '7878' },
+  { name: 'Sonarr', port: '8989' },
+  { name: 'Jackett', port: '9117' },
+];
